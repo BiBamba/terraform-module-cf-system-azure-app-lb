dc9f28d6e40d79e289d8f1c92ed3bbda53649084
Bi Bamba <maxime-tizie@hotmail.fr>
2022-11-21 11:13:15 -0600

    add new variables name

--- modules/application_gateway/variables.tf
+++ modules/application_gateway/variables.tf
@@ -1,3 +1,23 @@
+variable "resource_group_name" {
+  description = "This is the Resource Group Name"
+}
+
+variable "virtual_network_name" {
+  description = "This is the name of the virtual Network"
+}
+
+variable "resource_group_location" {
+  description = "This is the location(Region) of the Resource Group"
+}
+
+variable "frondend_subnet_name" {
+  description = "This is the name of the frondend Subnet where the Application Gateway resides"
+}
+
+variable "frondend_subnet_id" {
+  description = "This is the id of the Frontend Subnet"
+}
+
 variable "address_space" {
   default = "10.21.0.0/16"
 }
@@ -22,30 +42,86 @@ variable "https_frontend_port_name" {
   default = "AppGatW-Http-port"
 }
 
-variable "websockets_request_routing_rule_name" {
-  default = "AppGatW_Websockets-rule"
+variable "frontend_public_ip_name" {
+  description = "this is the name of the public ip for the Application gateway"
 }
 
-variable "frondend_subnet_name" {
-  description = "This is the name of the frondend Subnet where the Application Gateway resides"
-}
-
-variable "resource_group_name" {
-  description = "This is the Resource Group Name"
-}
-
-variable "virtual_network_name" {
-  description = "This is the name of the virtual Network"
-}
-
-variable "frontend_public_ip" {
-  description = "this is the public ip for the Application gateway"
-}
-
-variable "resource_group_location" {
-  description = "This is the location(Region) of the Resource Group"
+variable "frontend_public_ip_id" {
+  description = "This is teh id of the public ip of the Application Gateway"
 }
 
 variable "app_gateway_name" {
   description = "This is the name of the Application Gateway"
+}
+
+variable "gw_ip-configuration_name" {
+  description = "This is the name of the gateway ip configuration"
+}
+
+variable "sku_name " {
+  description = "The SKU name of WAF configuration"
+}
+
+variable "sku_tier" {
+  description = "The tier name of WAF configuration"
+}
+
+variable "sku_capacity" {
+  description = "The capacity of WAF configuration"
+}
+
+variable "min_capacity" {
+  description= "The minimum capacity in the autoscaling configuration"
+}
+
+variable "max_capacity" {
+  description= "The maximum capacity in the autoscaling configuration"
+}
+
+variable "probe_name" {
+  description = "The http probe name"
+}
+
+variable "https_probe_name" {
+  description = "The https probe name"
+}
+
+variable "backend_address_pool_name" {
+  description = "The name of the backend address pool"
+}
+
+variable "https_setting_name" {
+  description = "The name of the http setting for https " 
+}
+
+variable "http_setting_name" {
+  description = "The name of the http setting for http " 
+}
+
+variable "https_listener_name" {
+  description = "The name of the http listeners for https protocol"
+}
+
+variable "ssl_certificate_name" {
+  description = "The name of the ssl certificate of the https listeners"
+}
+
+variable "listener_name" {
+  description = "The name of the http listeners for http protocol"
+}
+
+variable "https_request_routing_rule_name" {
+  description = "The name of the https request rule"
+}
+
+variable "websockets_request_routing_rule_name" {
+  default = "AppGatW_Websockets-rule"
+}
+
+variable "redirect_configuration_name" {
+  description = "The name of redirect configuration for http to https "
+}
+
+variable "http_request_routing_rule_name" {
+  description = "The name of the http request rule"
 }
